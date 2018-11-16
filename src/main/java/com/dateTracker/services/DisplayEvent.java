c223e6fd5026131a2e876741fe60ed198caacfea
Kia Yang <kyang38@madisoncollege.edu>
2018-11-15 21:03:44 -0600

    updated jsp pages

--- src/main/java/com/dateTracker/services/DisplayEvent.java
+++ src/main/java/com/dateTracker/services/DisplayEvent.java
@@ -1,19 +1,18 @@
 package com.dateTracker.services;
 
 import com.dateTracker.entity.Event;
+import com.dateTracker.entity.User;
 import com.dateTracker.persistence.GenericDao;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.util.List;
-import java.util.logging.Logger;
-
-import static javax.ws.rs.client.Entity.entity;
 
 /**
  * This class is the Rest Service that has endpoints to get all events, get events by username,
@@ -22,16 +21,75 @@ import static javax.ws.rs.client.Entity.entity;
 
 @Path("/events")
 public class DisplayEvent {
+    private final Logger logger = LogManager.getLogger(this.getClass());
 
-    @POST
+    @GET
     @Path("/search/all")
     public Response getAllEvents() throws JsonProcessingException {
         GenericDao userDAO = new GenericDao(Event.class);
 
-        List<Event> usersList = userDAO.getAll();
-        ObjectMapper mapper = new ObjectMapper();
-        String responseString = mapper.writeValueAsString(usersList);
-        return Response.status(200).entity(responseString).build();
+        List<Event> eventsList = userDAO.getAll();
+        //ObjectMapper mapper = new ObjectMapper();
+        //String responseString = mapper.writeValueAsString(usersList);
+        //return Response.status(200).entity(responseString).build();
+
+        String stringResponse = "";
+        if (!eventsList.isEmpty()) {
+            try {
+                logger.info("starting the try block");
+                ObjectMapper mapper = new ObjectMapper();
+                stringResponse = mapper.writeValueAsString(eventsList);
+                logger.debug("in the try block and added parsed json for all EVENTS");
+            } catch (IOException ioException) {
+                logger.error(ioException.getMessage());
+                logger.info(ioException.getMessage());
+            }
+            logger.debug("string response: " + stringResponse);
+            return Response.status(200).entity(stringResponse).build();
+        } else {
+            return Response.status(404).entity("Status 404: Not Found").build();
+        }
+    }
+
+    @GET
+    @Path("/searchbyName/{userName}")
+    public Response getAllEvents(@PathParam("userName") String name) throws JsonProcessingException {
+        GenericDao eventDAO = new GenericDao(Event.class);
+        GenericDao userDAO = new GenericDao(User.class);
+        logger.info(name);
+        //instantiate and retrieves list of user object by username
+        logger.debug("getting list of users");
+        List<User> userList = userDAO.getByPropertyEqual("userName", name);
+        logger.info(userList);
+        //access list to get user object to get the id
+        // if userList.size() is greater than zero
+        logger.debug("getting user from list");
+        User user = userList.get(0);
+        //else
+        // return Response.status(404).entity("Status 404: Not Found").build();
+        logger.info(user);
+        logger.debug("getting users id");
+        int id = user.getId();
+        logger.info(id);
+        List<Event> eventsList = eventDAO.getByPropertyEqualint("user", id);
+        logger.info(eventsList);
+
+        String stringResponse = "";
+        if (!eventsList.isEmpty()) {
+            try {
+                logger.info("starting the try block");
+                ObjectMapper mapper = new ObjectMapper();
+                stringResponse = mapper.writeValueAsString(eventsList);
+                logger.debug("in the try block and added parsed json for all EVENTS");
+            } catch (IOException ioException) {
+                logger.error(ioException.getMessage());
+                logger.info(ioException.getMessage());
+            }
+            logger.debug("string response: " + stringResponse);
+            return Response.status(200).entity(stringResponse).build();
+        } else {
+            return Response.status(404).entity("Status 404: Not Found").build();
+        }
     }
 
     @POST
